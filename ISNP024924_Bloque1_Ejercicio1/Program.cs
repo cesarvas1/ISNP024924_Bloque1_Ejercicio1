b20fe9062a9438bcc733c76fe1b464cac74a99ad
César <Csar@DESKTOP-4INIV8U>
2024-10-20 18:03:58 -0600

    Agregar archivos de proyecto.

---
+++ ISNP024924_Bloque1_Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+
+        do
+        {
+            Console.Write("Ingrese un número (0 para salir): ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+
+        } while (numero != 0);
+
+        Console.WriteLine("Programa terminado.");
+    }
+}
